c2835a7cdf450695fa688e954f105ca1b259651c
yangarong <yanghj8562@gmail.com>
2020-01-10 13:46:23 +0900

    initall commit

---
+++ src/main/java/net/slipp/MySlippWindows10Application.java
@@ -0,0 +1,13 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippWindows10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippWindows10Application.class, args);
+	}
+
+}
